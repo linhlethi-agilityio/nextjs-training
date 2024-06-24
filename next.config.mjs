a510dabf612953f18aaa827001d4fcc72cc8cd1b
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-06-24 15:56:48 +0700

    init workspace

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  staticPageGenerationTimeout: 1000,
-};
-
-export default nextConfig;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Homepage = () => {
-  return (
-    <main className="container mx-auto">
-      <h1 className="text-lg font-semibold">This is homepage</h1>
-    </main>
-  );
-};
-
-export default Homepage;
