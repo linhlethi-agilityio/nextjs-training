f637470ca524f564c65776c31c94d6b05ea30dfa
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-07-05 14:09:15 +0700

    feat/remove-code-un-use

--- admin-dashboard/src/constants/index.ts
+++ admin-dashboard/src/constants/index.ts
@@ -2,7 +2,6 @@ export * from './menu';
 export * from './routes';
 export * from './messages';
 export * from './select';
-export * from './tab-label';
 export * from './api-endpoint';
 export * from './envs';
 export * from './filter';
